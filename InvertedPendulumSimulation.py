c94884302fa6f1de77d15af655638bf619d954d6
Manish Patel <redwall9@gmail.com>
2021-10-07 09:19:10 -0700

    Plot reference, actual, and estimated cart position. Switch to a sinusoidal reference trajectory.

--- InvertedPendulumSimulation.py
+++ InvertedPendulumSimulation.py
@@ -7,6 +7,7 @@ import scipy
 import scipy.linalg
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 # make numpy output a little easier to read
 np.set_printoptions(threshold=sys.maxsize)
@@ -32,13 +33,13 @@ L = 15
 # state covariance matrices. We use the continuous time random acceleration
 # model as defined in
 # https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7289336, where the
-# variance of the random acceleration is 0.001 m/s^2
+# variance of the random acceleration is 0.1 m/s^2
 V = np.array([
     [Ts**4 / 3, Ts**3 / 2, 0, 0],
     [Ts**3 / 2, Ts**2, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-]) * 0.001
+]) * 0.1
 
 # output / sensor covariance matrix. The first entry on the diagonal represents
 # the variance of the position sensor for the cart. A more intuituve way to look
@@ -58,7 +59,7 @@ W = np.array([
 # weight on the control effort (input)
 Q = np.zeros((4,4))
 Q[0][0] = 10
-Q[1][1] = 5
+Q[1][1] = 1
 Q[2][2] = 10
 Q[3][3] = 1
 
@@ -70,17 +71,24 @@ P0 = np.identity(4) * 0.1
 model = InvertedPendulumModel(0.5, 0.2, 0.1, 0.3, 0.006, x, u, V, W, Ts)
 ekf = EKF(model, x, P0)
 
-# generate the trajectory that we want to follow - a simple line where the cart
-# is moving at 0.05 m/s
-sim_length = 1000                   # in number of time steps
-x_ref = np.zeros((sim_length*4,1))
-for i in range(0, sim_length*4, 4):
-    x_ref[i][0]   = 0.05 * (Ts * i / 4)
-    x_ref[i+1][0] = 0.05
-    x_ref[i+2][0] = 0
-    x_ref[i+3][0] = 0
+sim_length = 1000 + L                   # in number of time steps
+
+# matrices to store the true state, estimated state, and reference trajectory
+x_true = np.empty((4, sim_length - L))
+x_hat = np.empty((4, sim_length - L))
+x_ref = np.empty((4, sim_length - L))
+
+# generate the trajectory that we want to follow
+for i in range(0, sim_length - L):
+    x_ref[0, i] = .125 * math.sin(math.pi * Ts * i / 5)
+    x_ref[1, i] = .125 * math.pi / 5 * math.cos(math.pi * Ts * i / 5)
+    x_ref[2, i] = 0
+    x_ref[3, i] = 0
 
 for i in range(sim_length - L):
+    x_true[:, i] = x[:,0]
+    x_hat[:, i] = ekf.x_hat[:,0]
+
     # update true model for system based on the currently applied input
     x = x + model.get_noisy_model_derivative(x, u)*Ts
 
@@ -88,7 +96,12 @@ for i in range(sim_length - L):
     ekf.predict_and_update(u, model.get_noisy_output(x))
 
     # compute new control input using model predictive control
-    u = np.array([[MPC(model, ekf.x_hat, x_ref[i*4:(i+L)*4,:], Q, R)[0]]])
+    u = np.array([[MPC(model, ekf.x_hat, x_ref[:,i:i+L].flatten("F").reshape(-1, 1), Q, R)[0]]])
 
-    print(x)
-    print(x_ref[i*4])
+# plot the results
+time = np.arange(0, Ts*(sim_length-L), Ts)
+
+plt.plot(time, x_true[0, :])
+plt.plot(time, x_ref[0, :])
+plt.plot(time, x_hat[0, :])
+plt.show()
